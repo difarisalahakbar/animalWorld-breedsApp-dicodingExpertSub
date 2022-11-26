3813165d752c51dc86940bde85cb07c81aa44e0d
Difa Risalah <dfgaming120201@gmail.com>
2022-11-26 11:48:14 +0700

    last update i hope

--- app/src/main/java/com/example/capstones/search/SearchActivity.kt
+++ app/src/main/java/com/example/capstones/search/SearchActivity.kt
@@ -1,17 +1,10 @@
 package com.example.capstones.search
 
-import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.EditorInfo
-import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
-import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
